fa17277f85acb0990f2b4bb39e7cf0fbf7031282
Nick <nick.joat@gmail.com>
2024-11-08 13:31:44 -0800

    created directory layout for all pages and components

---
+++ src/app/components/InsideBook/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InsideBook = () => {
+  return (
+    <div>InsideBook</div>
+  )
+}
+
+export default InsideBook
---
+++ src/app/components/PlayerBook/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlayerBook = () => {
+  return (
+    <div>PlayerBook</div>
+  )
+}
+
+export default PlayerBook

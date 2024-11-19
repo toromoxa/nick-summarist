a9662fb6ce842b7bd1e083e72730c86d4b91ecb2
Nick <nick.joat@gmail.com>
2024-11-19 10:42:27 -0800

    continued styling the search bar and side bar. preparing to wire up the logout button

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -3,25 +3,25 @@ import { FiSearch, FiMenu } from "react-icons/fi";
 
 const SearchBar: React.FC = () => {
   return (
-    <div className="search__background bg-[#fff] border-b-[#e1e7ea] h-[80px] z-[1]">
+    <div className="search__background bg-[#fff] border-b-[#e1e7ea] border-b-[1px] h-[80px] z-[1]">
       <div className="search__wrapper relative flex items-center justify-between px-8 max-w-[1070px] mx-auto h-full">
-        <figure>
-          <img src="logo.png" alt="summarist logo" />
+        <figure className="hidden md:invisible md:flex">
+          <img src="logo.png" alt="" />
         </figure>
-        <div className="search__content flex items-center gap-6 max-w-[340px] w-full">
+        <div className="search__content flex items-center gap-6 max-w-[340px] w-full mr-3">
           <div className="search flex items-center w-full">
-            <div className="search__input--wrapper relative gap-2">
+            <div className="search__input--wrapper relative gap-2 flex flex-row w-full text-sm">
               <input
                 type="text"
                 placeholder="Search for Books"
-                className="search__input h-[40px] w-full px-4 outline-none bg-[#f1f6f4] text-[#042330] border-2- border-solid border-[#e1e7ea] rounded-lg"
+                className="search__input h-[40px] w-full px-4 outline-none bg-[#f1f6f4] text-[#042330] border-2 border-solid border-[#e1e7ea] rounded-lg"
               />
               <div className="search__icon flex items-center absolute h-full right-2 justify-items-end border-l-2 border-solid border-[#e1e7ea] border-t-0 border-r-0 border-b-0 pl-2">
                 <FiSearch width={24} height={24} className="w-6 h-6 text-[#03314b]" />
               </div>
             </div>
           </div>
-          <button className="sidebar__toggle-btn hidden sm:flex items-center justify-center cursor-pointer">
+          <button className="sidebar__toggle-btn md:hidden sm:flex items-center justify-center cursor-pointer">
             <FiMenu className="w-6 h-6" />
           </button>
         </div>
--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,54 +1,89 @@
-import React from 'react';
-import { CiBookmark } from 'react-icons/ci';
-import { HiOutlineSearch } from 'react-icons/hi';
+import React from "react";
+import { CiBookmark } from "react-icons/ci";
+import { HiOutlineSearch } from "react-icons/hi";
+import { LuHelpCircle, LuLogOut } from "react-icons/lu";
 import { PiHouseLine } from "react-icons/pi";
-import { RiBallPenLine } from 'react-icons/ri';
-
+import { RiBallPenLine } from "react-icons/ri";
+import { RxGear } from "react-icons/rx";
 
 const SideBar = () => {
   return (
     <div>
-      <div className='sidebar__overlay opacity-0 pointer-events-none fixed top-0 left-0 w-full h-full bg-[#3a4649] z-10 transition-opacity ease-in-out duration-500'></div>
-      <div className="sidebar hidden lg:block bg-[#f7faf9] w-[200px] min-w-[200px] fixed top-0 left-0 h-[100vh] z-[1000] transition-all ease-in-out duration-300">
+      <div className="sidebar__overlay opacity-0 pointer-events-none fixed top-0 left-0 w-full h-full bg-[#3a4649] z-10 transition-opacity ease-in-out duration-500"></div>
+      <div className="sidebar hidden md:block bg-[#f7faf9] w-[200px] min-w-[200px] fixed top-0 left-0 h-[100vh] z-[1000] transition-all ease-in-out duration-300">
         <div className="sidebar__logo flex items-center justify-center h-[60px] pt-4 max-w-40 mx-auto">
-          <img src="logo.png" alt="" className='w-full h-10' />
+          <img src="logo.png" alt="" className="w-full h-10" />
         </div>
-        <div className="sidebar__wrapper flex flex-col justify-between h-[calc(100vh - 60px)] pb-5 overflow-y-auto">
-          <div className="sidebar__top flex mt-10">
-            <a href="/ForYou" className="sidebar__link--wrapper flex items-center h-[56px] text-[#032b41] mb-2 cursor-pointer">
+        <div className="sidebar__wrapper flex flex-col justify-between h-[90%] pb-3 overflow-y-auto">
+          <div className="sidebar__top flex flex-col mt-10">
+            <a
+              href="/ForYou"
+              className="sidebar__link--wrapper hover:bg-[#f0efef] flex items-center h-[56px] text-[#032b41] mb-2 cursor-pointer"
+            >
               <div className="sidebar__link--line active:bg-[#2bd97c] w-[6px] h-full mr-4"></div>
-              <div className="sidebar__icon--wrapper">
-                <PiHouseLine className='w-6 h-6' />
+              <div className="sidebar__icon--wrapper mr-2">
+                <PiHouseLine className="w-6 h-6" />
               </div>
               <div className="sidebar__link--text">For you</div>
             </a>
-            <a href="/Library" className="sidebar__link--wrapper flex items-center h-[56px] text-[#032b41] mb-2 cursor-pointer">
+            <a
+              href="/Library"
+              className="sidebar__link--wrapper hover:bg-[#f0efef] flex items-center h-[56px] text-[#032b41] mb-2 cursor-pointer"
+            >
               <div className="sidebar__link--line active:bg-[#2bd97c] w-[6px] h-full mr-4"></div>
-              <div className="sidebar__icon--wrapper">
-                <CiBookmark  className='w-6 h-6' />
+              <div className="sidebar__icon--wrapper mr-2">
+                <CiBookmark className="w-6 h-6" />
               </div>
               <div className="sidebar__link--text">My Library</div>
             </a>
-            <div className="sidebar__link--wrapper cursor-not-allowed flex items-center h-[56px] text-[#032b41] mb-2">
+            <div className="sidebar__link--wrapper hover:bg-[#f0efef] cursor-not-allowed flex items-center h-[56px] text-[#032b41] mb-2">
               <div className="sidebar__link--line active:bg-[#2bd97c] w-[6px] h-full mr-4"></div>
-              <div className="sidebar__icon--wrapper">
-                <RiBallPenLine className='w-6 h-6' />
+              <div className="sidebar__icon--wrapper mr-2">
+                <RiBallPenLine className="w-6 h-6" />
               </div>
               <div className="sidebar__link--text">Highlights</div>
             </div>
-            <div className="sidebar__link--wrapper cursor-not-allowed flex items-center h-[56px] text-[#032b41] mb-2">
+            <div className="sidebar__link--wrapper hover:bg-[#f0efef] cursor-not-allowed flex items-center h-[56px] text-[#032b41] mb-2">
               <div className="sidebar__link--line active:bg-[#2bd97c] w-[6px] h-full mr-4"></div>
-              <div className="sidebar__icon--wrapper">
-                <HiOutlineSearch className='w-6 h-6' />
+              <div className="sidebar__icon--wrapper mr-2">
+                <HiOutlineSearch className="w-6 h-6" />
               </div>
               <div className="sidebar__link--text">Search</div>
             </div>
           </div>
-          <div className="sidebar__bottom"></div>
+          <div className="sidebar__bottom">
+            <a
+              href="/ForYou"
+              className="sidebar__link--wrapper hover:bg-[#f0efef] flex items-center h-[56px] text-[#032b41] mb-2 cursor-pointer"
+            >
+              <div className="sidebar__link--line active:bg-[#2bd97c] w-[6px] h-full mr-4"></div>
+              <div className="sidebar__icon--wrapper mr-2">
+                <RxGear className="w-6 h-6" />
+              </div>
+              <div className="sidebar__link--text">Settings</div>
+            </a>
+            <div className="sidebar__link--wrapper hover:bg-[#f0efef] cursor-not-allowed flex items-center h-[56px] text-[#032b41] mb-2">
+              <div className="sidebar__link--line active:bg-[#2bd97c] w-[6px] h-full mr-4"></div>
+              <div className="sidebar__icon--wrapper mr-2">
+                <LuHelpCircle className="w-6 h-6" />
+              </div>
+              <div className="sidebar__link--text">Help & Support</div>
+            </div>
+            <a
+              href="/ForYou"
+              className="sidebar__link--wrapper hover:bg-[#f0efef] flex items-center h-[56px] text-[#032b41] mb-2 cursor-pointer"
+            >
+              <div className="sidebar__link--line active:bg-[#2bd97c] w-[6px] h-full mr-4"></div>
+              <div className="sidebar__icon--wrapper mr-2">
+                <LuLogOut className="w-6 h-6" />
+              </div>
+              <div className="sidebar__link--text">For you</div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
